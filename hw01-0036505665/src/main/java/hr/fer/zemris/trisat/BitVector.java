98d2d2487b05e881abd356abfdf6a9d941be62df
Bruna <brdujmovic@gmail.com>
2019-10-08 21:55:22 +0200

    Add javadoc for BitVector

--- hw01-0036505665/src/main/java/hr/fer/zemris/trisat/BitVector.java
+++ hw01-0036505665/src/main/java/hr/fer/zemris/trisat/BitVector.java
@@ -2,26 +2,62 @@ package hr.fer.zemris.trisat;
 
 import java.util.Random;
 
+/**
+ * This class is an immutable representation of a 3-SAT problem solution.
+ *
+ * The size of the vector determines the number of variables of the problem.
+ *
+ * Each bit in the vector represents the boolean value of a given variable.
+ * The leftmost bit is for the variable x1, the next one is for x2, etc.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public class BitVector {
 
+    /**
+     * Constructs a {@link BitVector} of random boolean values.
+     *
+     * @param rand the object for generating random values
+     * @param numberOfBits the size of the vector
+     */
     public BitVector(Random rand, int numberOfBits) {
 
     }
 
+    /**
+     * Constructs a {@link BitVector} of the given values.
+     *
+     * @param bits the boolean values of the vector
+     */
     public BitVector(boolean... bits) {
 
     }
 
+    /**
+     * Constructs a {@link BitVector} of the given size with all bits set to zero.
+     *
+     * @param n the size of the vector
+     */
     public BitVector(int n) {
 
     }
 
-    // vraća vrijednost index-te varijable
+    /**
+     * Returns the boolean value of the index-th variable.
+     *
+     * @param index the index of the variable whose value should be returned
+     * @return the boolean value of the index-th variable
+     */
     public boolean get(int index) {
         return false;
     }
 
-    // vraća broj varijabli koje predstavlja
+    /**
+     * Returns the number of variables represented by this vector.
+     *
+     * @return the number of variables represented by this vector
+     */
     public int getSize() {
         return -1;
     }
@@ -31,7 +67,11 @@ public class BitVector {
         return null;
     }
 
-    // vraća promjenjivu kopiju trenutnog rješenja
+    /**
+     * Returns a mutable copy of this vector.
+     *
+     * @return a mutable copy of this vector
+     */
     public MutableBitVector copy() {
         return null;
     }
