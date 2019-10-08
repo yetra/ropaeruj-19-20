522e23fdf77aa831ec48fb78fd2068328e79189a
Bruna <brdujmovic@gmail.com>
2019-10-08 22:54:27 +0200

    Implement Clause methods

--- hw01-0036505665/src/main/java/hr/fer/zemris/trisat/Clause.java
+++ hw01-0036505665/src/main/java/hr/fer/zemris/trisat/Clause.java
@@ -1,5 +1,7 @@
 package hr.fer.zemris.trisat;
 
+import java.util.Arrays;
+
 /**
  * This class represents a sum of literals (variables or their complements) of a formula in conjunctive normal form.
  *
@@ -9,12 +11,18 @@ package hr.fer.zemris.trisat;
 public class Clause {
 
     /**
+     * An array of this clause's literals represented by their indexes.
+     * Negative indexes represent variable complements.
+     */
+    private int[] literals;
+
+    /**
      * Constructs a clause containing literals specified by the given indexes.
      *
      * @param indexes the indexes of literals in this clause
      */
     public Clause(int[] indexes) {
-
+        literals = indexes;
     }
 
     /**
@@ -23,17 +31,22 @@ public class Clause {
      * @return the number of literals in this clause
      */
     public int getSize() {
-        return -1;
+        return literals.length;
     }
 
     /**
      * Returns the index of the literal that is on the index-th position in this clause.
      *
      * @param index the position of the literal
+     * @throws IllegalArgumentException if the given index is not in range [1, size]
      * @return the index of the literal that is on the index-th position in this clause
      */
     public int getLiteral(int index) {
-        return -1;
+        if (index < 1 || index > getSize()) {
+            throw new IllegalArgumentException("Index must be in range [1, size]");
+        }
+
+        return literals[index - 1];
     }
 
     /**
@@ -43,11 +56,17 @@ public class Clause {
      * @return {@code true} if the given bit assignment satisfies this clause
      */
     public boolean isSatisfied(BitVector assignment) {
-        return false;
+        boolean result = false;
+
+        for (int index : literals) {
+            result |= assignment.get(Math.abs(index));
+        }
+
+        return result;
     }
 
     @Override
     public String toString() {
-        return null;
+        return Arrays.toString(literals).replace(", ", " + ");
     }
 }
