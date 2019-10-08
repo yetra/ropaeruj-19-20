ad97d76bfa64d5e9a9ff966a3d29f50402ebb4a0
Bruna <brdujmovic@gmail.com>
2019-10-08 23:09:01 +0200

    Implement SATFormula methods

--- hw01-0036505665/src/main/java/hr/fer/zemris/trisat/SATFormula.java
+++ hw01-0036505665/src/main/java/hr/fer/zemris/trisat/SATFormula.java
@@ -1,5 +1,7 @@
 package hr.fer.zemris.trisat;
 
+import java.util.Arrays;
+
 /**
  * This class represents a formula in conjunctive normal form.
  *
@@ -9,13 +11,24 @@ package hr.fer.zemris.trisat;
 public class SATFormula {
 
     /**
+     * The number of variables in this formula.
+     */
+    private int numberOfVariables;
+
+    /**
+     * The clauses comprising this formula.
+     */
+    private Clause[] clauses;
+
+    /**
      * Constructs a {@link SATFormula} of the given clauses and number of variables.
      *
      * @param numberOfVariables the number of variables in the formula
      * @param clauses the clauses comprising the formula
      */
     public SATFormula(int numberOfVariables, Clause[] clauses) {
-
+        this.numberOfVariables = numberOfVariables;
+        this.clauses = clauses;
     }
 
     /**
@@ -24,7 +37,7 @@ public class SATFormula {
      * @return the number of variables in this formula
      */
     public int getNumberOfVariables() {
-        return -1;
+        return numberOfVariables;
     }
 
     /**
@@ -33,31 +46,42 @@ public class SATFormula {
      * @return the number of clauses in this formula
      */
     public int getNumberOfClauses() {
-        return -1;
+        return clauses.length;
     }
 
     /**
      * Returns the clause that is on the specified index in this formula.
      *
      * @param index the index of the clause
+     * @throws IllegalArgumentException if the given index is not in range [1, size]
      * @return the clause that is on the specified index in this formula
      */
     public Clause getClause(int index) {
-        return null;
+        if (index < 1 || index > getNumberOfClauses()) {
+            throw new IllegalArgumentException("Index must be in range [1, size]");
+        }
+
+        return clauses[index - 1];
     }
 
     /**
      * Returns {@code true} if the given bit assignment satisfies this formula.
      *
      * @param assignment the bit assignment to check
-     * @return if the given bit assignment satisfies this formula
+     * @return {@code true} if the given bit assignment satisfies this formula
      */
     public boolean isSatisfied(BitVector assignment) {
-        return false;
+        boolean result = true;
+
+        for (Clause clause : clauses) {
+            result &= clause.isSatisfied(assignment);
+        }
+
+        return result;
     }
 
     @Override
     public String toString() {
-        return null;
+        return Arrays.toString(clauses).replace(", ", "").replace("[[", "[").replace("]]", "]");
     }
 }
