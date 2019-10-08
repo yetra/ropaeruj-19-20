fbe3236dc69322cc0ba1c7f5dec6468f76d6bc95
Bruna <brdujmovic@gmail.com>
2019-10-08 21:36:56 +0200

    Add BitVector class outline

---
+++ hw01-0036505665/src/main/java/hr/fer/zemris/trisat/BitVector.java
@@ -0,0 +1,38 @@
+package hr.fer.zemris.trisat;
+
+import java.util.Random;
+
+public class BitVector {
+
+    public BitVector(Random rand, int numberOfBits) {
+
+    }
+
+    public BitVector(boolean... bits) {
+
+    }
+
+    public BitVector(int n) {
+
+    }
+
+    // vraća vrijednost index-te varijable
+    public boolean get(int index) {
+        return false;
+    }
+
+    // vraća broj varijabli koje predstavlja
+    public int getSize() {
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    // vraća promjenjivu kopiju trenutnog rješenja
+    public MutableBitVector copy() {
+        return null;
+    }
+}
