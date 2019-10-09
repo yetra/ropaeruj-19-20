7ec605a9a371701dab8bf06a4929c5184aca29c5
Bruna <brdujmovic@gmail.com>
2019-10-10 00:34:53 +0200

    Add javadoc for Algorithm1 class

--- hw01-0036505665/src/main/java/hr/fer/zemris/trisat/algorithms/Algorithm1.java
+++ hw01-0036505665/src/main/java/hr/fer/zemris/trisat/algorithms/Algorithm1.java
@@ -3,16 +3,46 @@ package hr.fer.zemris.trisat.algorithms;
 import hr.fer.zemris.trisat.BitVector;
 import hr.fer.zemris.trisat.SATFormula;
 
+/**
+ * This class represents an exhaustive search algorithm for solving the 3-SAT problem.
+ *
+ * Possible solutions are generated from binary representations of integers
+ * in range [0, 2^m], where m is the number of variables in the formula.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public class Algorithm1 {
 
+    /**
+     * The formula whose solution should be found.
+     */
     private SATFormula formula;
 
+    /**
+     * The solution of the 3-SAT problem.
+     */
     private BitVector solution;
 
+    /**
+     * Constructs an {@link Algorithm1} object for the given formula.
+     *
+     * @param formula the formula whose solution should be found
+     */
     public Algorithm1(SATFormula formula) {
         this.formula = formula;
     }
 
+    /**
+     * Executes the algorithm on the given formula.
+     *
+     * Returns a solution if found, otherwise returns {@code null}.
+     * If multiple solutions are found, the first solution will be returned.
+     *
+     * All solutions are printed to the console.
+     *
+     * @return a solution if found, else {@code null}
+     */
     public BitVector execute() {
         double maximumValue = Math.pow(2, formula.getNumberOfClauses());
         int currentValue = 0;
