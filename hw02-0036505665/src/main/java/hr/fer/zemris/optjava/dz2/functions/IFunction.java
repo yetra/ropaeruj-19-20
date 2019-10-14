06348d6f19814b28b2f106923b3d49ea86e9a27a
Bruna <brdujmovic@gmail.com>
2019-10-14 21:21:10 +0200

    Add IFunction interface

---
+++ hw02-0036505665/src/main/java/hr/fer/zemris/optjava/dz2/functions/IFunction.java
@@ -0,0 +1,35 @@
+package hr.fer.zemris.optjava.dz2.functions;
+
+import org.apache.commons.math3.linear.RealVector;
+
+/**
+ * Models an arbitrary function of n real variables.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public interface IFunction {
+
+    /**
+     * Returns the number of variables of this function.
+     *
+     * @return the number of variables of this function
+     */
+    int getNumberOfVariables();
+
+    /**
+     * Returns the value of this function in the specified point.
+     *
+     * @param point the point for calculating the value
+     * @return the value of this function in the specified point
+     */
+    double getValueOf(RealVector point);
+
+    /**
+     * Returns the value of the gradient of this function in the specified point.
+     *
+     * @param point the point for calculating the value of the gradient
+     * @return the value of the gradient of this function in the specified point
+     */
+    double getGradientIn(RealVector point);
+}
