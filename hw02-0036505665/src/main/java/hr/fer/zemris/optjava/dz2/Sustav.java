f5b0b0ba863982ce749a3b79e3c2839dc8412f50
Bruna <brdujmovic@gmail.com>
2019-10-16 00:04:08 +0200

    Add program for solving linear systems using num opt

---
+++ hw02-0036505665/src/main/java/hr/fer/zemris/optjava/dz2/Sustav.java
@@ -0,0 +1,67 @@
+package hr.fer.zemris.optjava.dz2;
+
+import hr.fer.zemris.optjava.dz2.algorithms.NumOptAlgorithms;
+import hr.fer.zemris.optjava.dz2.functions.LinearSystemFunction;
+import org.apache.commons.math3.linear.RealVector;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * This program uses the implemented {@link NumOptAlgorithms} for solving a system of linear equations.
+ *
+ * It accepts 3 command-line arguments: algorithm name ("grad" or "newton"), the maximum number of iterations,
+ * and the path to a file describing the linear system.
+ *
+ * The results are printed to the console.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class Sustav {
+
+    /**
+     * The main method. Uses {@link NumOptAlgorithms} for solving a linear system.
+     *
+     * @param args the command-line arguments, 3 expected
+     */
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Expected 3 arguments, got " + args.length);
+            System.exit(1);
+        }
+
+        String algorithmName = args[0];
+        int maxTries = Integer.parseInt(args[1]);
+        Path filePath = Paths.get(args[2]);
+
+        LinearSystemFunction function = null;
+        try {
+            function = LinearSystemFunction.fromFile(filePath);
+
+        } catch (IOException e) {
+            System.out.println("An I/O error occured!");
+            System.exit(1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("File is incorrectly formatted!");
+            System.exit(1);
+        }
+
+        RealVector solution = null;
+        switch (algorithmName) {
+            case "grad":
+                solution = NumOptAlgorithms.gradientDescent(function, maxTries);
+                break;
+            case "newton":
+                solution = NumOptAlgorithms.newtonsMethod(function, maxTries);
+                break;
+            default:
+                System.out.println("Unknown algorithm name " + algorithmName);
+                System.exit(1);
+        }
+
+        System.out.println("\nSolution: " + solution);
+        System.out.println("Error value: " + function.getValueOf(solution));
+    }
+}
