48a9f6e9b7de5737beb0d4a771a4a80fd97749a9
Bruna <brdujmovic@gmail.com>
2019-10-16 00:03:28 +0200

    Add method for parsing LinearSystemFunction from file

--- hw02-0036505665/src/main/java/hr/fer/zemris/optjava/dz2/functions/LinearSystemFunction.java
+++ hw02-0036505665/src/main/java/hr/fer/zemris/optjava/dz2/functions/LinearSystemFunction.java
@@ -1,7 +1,14 @@
 package hr.fer.zemris.optjava.dz2.functions;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 /**
  * A an implementation of {@link IHFunction} for solving a system of linear equations using numerical
@@ -60,5 +67,35 @@ public class LinearSystemFunction implements IHFunction {
     public RealMatrix getHessianIn(RealVector point) {
         // hessian F(x) = gradient of gradient F(x) = 2 * A^T * A
         return coefficients.transpose().scalarMultiply(2.0).multiply(coefficients);
+    }
+
+    /**
+     * Returns a {@link LinearSystemFunction} parsed from a given file.
+     *
+     * @param filePath a path to the file to parse
+     * @return a {@link LinearSystemFunction} parsed from the given file
+     * @throws IOException if an I/O error occurs while reading
+     * @throws IllegalArgumentException if the file is incorrectly formatted
+     */
+    public static LinearSystemFunction fromFile(Path filePath) throws IOException {
+        List<String> lines = Files.readAllLines(filePath);
+        lines.removeIf(line -> line.startsWith("#"));
+        int numberOfVariables = lines.size();
+
+        double[][] coefficients = new double[numberOfVariables][numberOfVariables];
+        double[] constants = new double[numberOfVariables];
+
+        for (int i = 0; i < numberOfVariables; i++) {
+            String[] parts = lines.get(i).substring(1, lines.get(i).length() - 1).split(", ");
+
+            for (int j = 0; j < parts.length - 1; j++) {
+                coefficients[i][j] = Double.parseDouble(parts[j]);
+            }
+            constants[i] = Double.parseDouble(parts[parts.length - 1]);
+        }
+
+        return new LinearSystemFunction(
+                new Array2DRowRealMatrix(coefficients), new ArrayRealVector(constants)
+        );
     }
 }
