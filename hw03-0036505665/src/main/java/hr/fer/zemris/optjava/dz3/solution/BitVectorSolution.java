739449fa0febdb7d97e32331fb5b7430145a1854
Bruna <brdujmovic@gmail.com>
2019-10-23 16:00:10 +0200

    Add BitVectorSolution class outline

---
+++ hw03-0036505665/src/main/java/hr/fer/zemris/optjava/dz3/solution/BitVectorSolution.java
@@ -0,0 +1,57 @@
+package hr.fer.zemris.optjava.dz3.solution;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Models an optimization algorithm solution that is based on a bit vector.
+ *
+ * @author Bruna Dujmović
+ */
+public class BitVectorSolution extends SingleObjectiveSolution {
+
+    /**
+     * An array of bits representing this solution.
+     */
+    private boolean[] bits;
+
+    /**
+     * Constructs a {@link BitVectorSolution} of the specified size with all the bits
+     * set to 0.
+     *
+     * @param size the size of the solution (the length of the bit array)
+     */
+    public BitVectorSolution(int size) {
+    }
+
+    /**
+     * Constructs a {@link BitVectorSolution} out of the given boolean array.
+     *
+     * @param bits the boolean array representing the solution
+     */
+    public BitVectorSolution(boolean[] bits) {
+    }
+
+    /**
+     * Returns the array of bits representing this solution.
+     *
+     * @return the array of bits representing this solution
+     */
+    public boolean[] getBits() {
+        return null;
+    }
+
+    /**
+     * Returns a new {@link BitVectorSolution} that is a duplicate of this solution.
+     *
+     * @return a new {@link BitVectorSolution} that is a duplicate of this solution
+     */
+    public BitVectorSolution duplicate() {
+        return null;
+    }
+
+    /**
+     * Randomizes the bits of this solution.
+     */
+    public void randomize() {
+    }
+}
