d876a452c52155facc64940af44805e48f4365ad
Bruna <brdujmovic@gmail.com>
2019-10-25 23:24:54 +0200

    Add IFunction interface

---
+++ hw04-0036505665/src/main/java/hr/fer/zemris/optjava/dz4/part1/IFunction.java
@@ -0,0 +1,18 @@
+package hr.fer.zemris.optjava.dz4.part1;
+
+/**
+ * Models an arbitrary function of n real variables.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public interface IFunction {
+
+    /**
+     * Returns the value of this function at the specified point.
+     *
+     * @param point the point for calculating the value
+     * @return the value of this function in the specified point
+     */
+    double valueAt(double[] point);
+}
