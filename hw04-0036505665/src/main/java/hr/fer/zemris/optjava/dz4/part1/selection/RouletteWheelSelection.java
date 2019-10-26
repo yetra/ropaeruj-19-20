4e2bdf348af9ecfd2239c0b6e19be1e04ea23696
Bruna <brdujmovic@gmail.com>
2019-10-26 13:07:27 +0200

    Add RouletteWheelSelection class

---
+++ hw04-0036505665/src/main/java/hr/fer/zemris/optjava/dz4/part1/selection/RouletteWheelSelection.java
@@ -0,0 +1,19 @@
+package hr.fer.zemris.optjava.dz4.part1.selection;
+
+import hr.fer.zemris.optjava.dz4.part1.Chromosome;
+
+import java.util.Collection;
+
+/**
+ * An implementation of roulette wheel selection.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class RouletteWheelSelection implements ISelection {
+
+    @Override
+    public Chromosome from(Collection<Chromosome> population) {
+        return null;
+    }
+}
