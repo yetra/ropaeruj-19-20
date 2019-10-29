5ce15ff2bad2391607739456454e4b52104bcc0f
Bruna <brdujmovic@gmail.com>
2019-10-29 17:46:15 +0100

    Implement roulette wheel selection

--- hw04-0036505665/src/main/java/hr/fer/zemris/optjava/dz4/part1/selection/RouletteWheelSelection.java
+++ hw04-0036505665/src/main/java/hr/fer/zemris/optjava/dz4/part1/selection/RouletteWheelSelection.java
@@ -3,6 +3,7 @@ package hr.fer.zemris.optjava.dz4.part1.selection;
 import hr.fer.zemris.optjava.dz4.part1.Chromosome;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * An implementation of roulette wheel selection.
@@ -14,6 +15,28 @@ public class RouletteWheelSelection implements ISelection {
 
     @Override
     public Chromosome from(List<Chromosome> population) {
-        return null;
+        double fitnessSum = 0;
+        double maxFitness = population.get(0).fitness;
+
+        for (Chromosome chromosome : population) {
+            fitnessSum += chromosome.fitness;
+
+            if (maxFitness < chromosome.fitness) {
+                maxFitness = chromosome.fitness;
+            }
+        }
+        fitnessSum = population.size() * maxFitness - fitnessSum;
+
+        double accumulatedSum = 0;
+        double randomNumber = ThreadLocalRandom.current().nextDouble() * fitnessSum;
+        for (Chromosome chromosome : population) {
+            accumulatedSum += maxFitness - chromosome.fitness;
+
+            if (randomNumber < accumulatedSum) {
+                return chromosome;
+            }
+        }
+
+        return population.get(population.size() - 1);
     }
 }
