e5a20f4937c688099a4b6f0e8a025f022542c524
Bruna <brdujmovic@gmail.com>
2019-10-29 14:18:11 +0100

    Implement BLX-alpha crossover

---
+++ hw04-0036505665/src/main/java/hr/fer/zemris/optjava/dz4/part1/crossover/BLXAlphaCrossover.java
@@ -0,0 +1,45 @@
+package hr.fer.zemris.optjava.dz4.part1.crossover;
+
+import hr.fer.zemris.optjava.dz4.part1.Chromosome;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * An implementation of BLX-alpha crossover.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class BLXAlphaCrossover implements ICrossover {
+
+    /**
+     * The alpha parameter of the crossover.
+     */
+    private double alpha;
+
+    /**
+     * Constructs a {@link BLXAlphaCrossover} of the given parameter.
+     *
+     * @param alpha the alpha parameter of the crossover
+     */
+    public BLXAlphaCrossover(double alpha) {
+        this.alpha = alpha;
+    }
+
+    @Override
+    public Collection<Chromosome> of(Chromosome firstParent, Chromosome secondParent) {
+        Chromosome child = new Chromosome(firstParent.values.length);
+
+        for (int i = 0; i < firstParent.values.length; i++) {
+            double minValue = Math.min(firstParent.values[i], secondParent.values[i]);
+            double maxValue = Math.min(firstParent.values[i], secondParent.values[i]);
+            double delta = maxValue - minValue;
+
+            child.values[i] = ThreadLocalRandom.current().nextDouble(minValue - delta * alpha, maxValue + delta * alpha);
+        }
+
+        return Collections.singletonList(child);
+    }
+}
