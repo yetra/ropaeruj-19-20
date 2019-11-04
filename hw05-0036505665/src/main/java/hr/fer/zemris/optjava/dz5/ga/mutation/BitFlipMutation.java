648181e6ebebf54963b0f8e20d9fc05f536ce837
Bruna <brdujmovic@gmail.com>
2019-11-04 14:28:03 +0100

    Add BitFlipMutation class

---
+++ hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/ga/mutation/BitFlipMutation.java
@@ -0,0 +1,23 @@
+package hr.fer.zemris.optjava.dz5.ga.mutation;
+
+import hr.fer.zemris.optjava.dz5.ga.Chromosome;
+
+/**
+ * An {@link IMutation} implementation that flips the bits of a given {@link Chromosome}
+ * with the given probability.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class BitFlipMutation implements IMutation {
+
+    @Override
+    public Chromosome of(Chromosome chromosome) {
+        return null;
+    }
+
+    @Override
+    public void mutate(Chromosome chromosome) {
+
+    }
+}
