83f931a3dde3cae44a52ee3bd9b3e027c8597373
Bruna <brdujmovic@gmail.com>
2019-11-04 21:32:36 +0100

    Add AbstractCompFactor class

---
+++ hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/part1/AbstractCompFactor.java
@@ -0,0 +1,33 @@
+package hr.fer.zemris.optjava.dz5.part1;
+
+/**
+ * An abstract class to be implemented by various comparison factor change plans.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public abstract class AbstractCompFactor {
+
+    /**
+     * The current comparison factor.
+     */
+    private double factor;
+
+    /**
+     * Constructs a comparison factor object of the given initial value.
+     *
+     * @param factor the initial value of the comparison factor (double between 0 and 1)
+     */
+    public AbstractCompFactor(double factor) {
+        this.factor = factor;
+    }
+
+    /**
+     * Returns the current comparison factor.
+     *
+     * @return the current comparison factor
+     */
+    public double getFactor() {
+        return factor;
+    }
+}
