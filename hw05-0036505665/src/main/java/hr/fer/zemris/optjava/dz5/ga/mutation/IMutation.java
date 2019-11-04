4145f8b93863716e1e7c795f6a71e76303c003fa
Bruna <brdujmovic@gmail.com>
2019-11-04 13:49:25 +0100

    Add IMutation interface

---
+++ hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/ga/mutation/IMutation.java
@@ -0,0 +1,28 @@
+package hr.fer.zemris.optjava.dz5.ga.mutation;
+
+import hr.fer.zemris.optjava.dz5.ga.Chromosome;
+
+/**
+ * An interface to be implemented by different types of GA mutation.
+ * Each implementation should provide a method for mutating a signle chromosome.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public interface IMutation {
+
+    /**
+     * Returns a mutated copy of the given chromosome.
+     *
+     * @param chromosome the chromosome to mutate
+     * @return a mutated copy of the given chromosome
+     */
+    Chromosome of(Chromosome chromosome);
+
+    /**
+     * Mutates the given chromosome.
+     *
+     * @param chromosome the chromosome to mutate
+     */
+    void mutate(Chromosome chromosome);
+}
