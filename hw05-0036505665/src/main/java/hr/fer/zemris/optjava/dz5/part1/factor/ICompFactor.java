3048a908d36aa75e8038bccd67d314d431d77217
Bruna <brdujmovic@gmail.com>
2019-11-04 21:39:40 +0100

    Change AbstractCompFactor class to ICompFactor interface

---
+++ hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/part1/factor/ICompFactor.java
@@ -0,0 +1,17 @@
+package hr.fer.zemris.optjava.dz5.part1.factor;
+
+/**
+ * An interface to be implemented by various comparison factor change plans.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public interface ICompFactor {
+
+    /**
+     * Returns the current comparison factor.
+     *
+     * @return the current comparison factor
+     */
+    double getFactor();
+}
