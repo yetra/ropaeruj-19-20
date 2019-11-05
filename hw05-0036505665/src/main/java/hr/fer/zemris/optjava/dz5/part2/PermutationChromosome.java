b4d7b4795c2e2cbfb44f98e80eb4e91d378c9499
Bruna <brdujmovic@gmail.com>
2019-11-05 18:13:26 +0100

    Add PermutationChromosome class

---
+++ hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/part2/PermutationChromosome.java
@@ -0,0 +1,56 @@
+package hr.fer.zemris.optjava.dz5.part2;
+
+import hr.fer.zemris.optjava.dz5.ga.Chromosome;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * This class represents a chromosome based on an array of integers that will be used in solving
+ * the Quadratic Assignment Problem.
+ *
+ * The allowed integer values are in range [0, {@link #values}.length].
+ * A single value can occur only once in the {@link #values} array.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class PermutationChromosome extends Chromosome<Integer> {
+
+    /**
+     * Constructs a random {@link PermutationChromosome} of the specified size.
+     *
+     * @param size the size of the chromosome
+     */
+    public PermutationChromosome(int size) {
+        values = new Integer[size];
+
+        randomize();
+    }
+
+    /**
+     * Constructs a {@link PermutationChromosome} of the given values.
+     *
+     * @param values the values of the chromosome
+     */
+    public PermutationChromosome(Integer[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public void randomize() {
+        for (int i = 0; i < values.length; i++) {
+            values[i] = ThreadLocalRandom.current().nextInt();
+        }
+    }
+
+    @Override
+    public Chromosome<Integer> copy() {
+        return new PermutationChromosome(Arrays.copyOf(values, values.length));
+    }
+
+    @Override
+    public void calculateFitness() {
+
+    }
+}
