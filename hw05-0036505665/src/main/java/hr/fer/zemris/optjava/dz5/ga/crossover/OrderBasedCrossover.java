4bf1c1607edc63793dc68b37e72f4c8fca656bab
Bruna <brdujmovic@gmail.com>
2019-11-07 10:09:38 +0100

    Fix order-based crossover

--- hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/ga/crossover/OrderBasedCrossover.java
+++ hw05-0036505665/src/main/java/hr/fer/zemris/optjava/dz5/ga/crossover/OrderBasedCrossover.java
@@ -2,21 +2,16 @@ package hr.fer.zemris.optjava.dz5.ga.crossover;
 
 import hr.fer.zemris.optjava.dz5.ga.chromosome.Chromosome;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * An implementation of order-based crossover that results in two child chromosomes.
+ * An implementation of order-based crossover (OX2) that results in two child chromosomes.
  *
- * A number of random positions are selected in the parents' values array. Each child
- * chromosome has the same values as one of the parents, except on the selected positions.
- * The first child will be filled with the second parent's values on those positions,
- * and the second child will be filled with the first parent's values on those positions.
+ * @author Bruna Dujmović
  *
- * @param <T>
  */
-public class OrderBasedCrossover<T> implements ICrossover<T> {
+public class OrderBasedCrossover implements ICrossover<Integer> {
 
     /**
      * The number of random positions to select.
@@ -24,21 +19,44 @@ public class OrderBasedCrossover<T> implements ICrossover<T> {
     private static final int NUM_OF_POSITIONS = 3;
 
     @Override
-    public Collection<Chromosome<T>> of(Chromosome<T> firstParent, Chromosome<T> secondParent) {
-        int[] positions = new int[NUM_OF_POSITIONS];
+    public Collection<Chromosome<Integer>> of(Chromosome<Integer> firstParent, Chromosome<Integer> secondParent) {
+        Set<Integer> positionsSet = new HashSet<>();
+        while (positionsSet.size() < NUM_OF_POSITIONS) {
+            positionsSet.add(ThreadLocalRandom.current().nextInt(firstParent.values.length));
+        }
+        Integer[] positions = positionsSet.toArray(new Integer[0]);
+
+        Chromosome<Integer> firstChild = firstParent.copy();
+        Chromosome<Integer> secondChild = secondParent.copy();
+
+        List<Integer> selectedInFirst = new ArrayList<>(NUM_OF_POSITIONS);
+        List<Integer> selectedInSecond = new ArrayList<>(NUM_OF_POSITIONS);
 
         for (int i = 0; i < NUM_OF_POSITIONS; i++) {
-            positions[i] = ThreadLocalRandom.current().nextInt(firstParent.values.length);
+            selectedInFirst.add(firstParent.values[positions[i]]);
+            selectedInSecond.add(secondParent.values[positions[i]]);
         }
 
-        Chromosome<T> firstChild = firstParent.copy();
-        Chromosome<T> secondChild = secondParent.copy();
-
-        for (int i = 0; i < NUM_OF_POSITIONS; i++) {
-            firstChild.values[positions[i]] = secondParent.values[positions[i]];
-            secondChild.values[positions[i]] = firstParent.values[positions[i]];
-        }
+        fillWithSelected(firstChild, selectedInSecond);
+        fillWithSelected(secondChild, selectedInFirst);
 
         return Arrays.asList(firstChild, secondChild);
+    }
+
+    /**
+     * Finds the positions in the child that contain elements from {@code selected} and
+     * fills them in the order in which the elements appear in {@code selected}.
+     *
+     * @param child the child to fill
+     * @param selected the selected values
+     */
+    private void fillWithSelected(Chromosome<Integer> child, List<Integer> selected) {
+        int indexOfSelected = 0;
+
+        for (int i = 0; i < child.values.length && indexOfSelected < NUM_OF_POSITIONS; i++) {
+            if (selected.contains(child.values[i])) {
+                child.values[i] = selected.get(indexOfSelected++);
+            }
+        }
     }
 }
