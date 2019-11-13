23d4ebb0e791fb2f517fdf775368d7372705c0cd
yetra <brdujmovic@gmail.com>
2019-11-13 21:35:32 +0100

    Add City javadoc

--- hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/City.java
+++ hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/City.java
@@ -1,16 +1,46 @@
 package hr.fer.zemris.optjava.dz6;
 
+/**
+ * Models a TSP city.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public class City {
+    /**
+     * The index of this city.
+     */
     public int index;
+
+    /**
+     * The x-coordinate of this city.
+     */
     public int x;
+
+    /**
+     * The y-coordinate of this city.
+     */
     public int y;
 
+    /**
+     * Constructs a {@link City} of the given arguments.
+     *
+     * @param index the index of this city
+     * @param x the x-coordinate of this city
+     * @param y the y-coordinate of this city
+     */
     public City(int index, int x, int y) {
         this.index = index;
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns the Euclidean distance from this city to a given other city.
+     *
+     * @param other the other city
+     * @return the Euclidean distance from this city to a given other city
+     */
     public double distanceTo(City other) {
         return Math.sqrt((this.x - other.x) * (this.x - other.x) + (this.y - other.y) * (this.y - other.y));
     }
