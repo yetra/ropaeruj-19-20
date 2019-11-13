6045037274dc70d87f3967189751191770deaa58
yetra <brdujmovic@gmail.com>
2019-11-13 21:42:22 +0100

    Add TSPSolver program

---
+++ hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolver.java
@@ -0,0 +1,54 @@
+package hr.fer.zemris.optjava.dz6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * A program for solving the specified TSP using the Max-Min Ant System algorithm.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class TSPSolver {
+
+    /**
+     * The main method. Reads TSP data and runs the MMAS algorithm.
+     *
+     * @param args the command-line arguments, 4 expected - a path to the TSP data file,
+     *             the number of closest city neighbors to find, the size of the ant population,
+     *             the maximum number of iterations before the algorithm terminates
+     */
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("Expected 4 arguments, got " + args.length);
+            System.exit(1);
+        }
+
+        int closestCount = Integer.parseInt(args[1]);
+        int antsCount = Integer.parseInt(args[2]);
+        int maxIterations = Integer.parseInt(args[3]);
+
+        List<City> cities = null;
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(args[0]))) {
+            cities = parseCities(br);
+
+        } catch (IOException e) {
+            System.out.println("An I/O exception occured!");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Parses a TSP data file using the given {@link BufferedReader}.
+     *
+     * @param br the {@link BufferedReader} to use
+     * @return a list of parsed cities
+     * @throws IOException if an I/O error occurs
+     */
+    private static List<City> parseCities(BufferedReader br) throws IOException {
+        return null;
+    }
+}
