9fb0f1ec42ccb2b83f06a800f936d080139bcd64
yetra <brdujmovic@gmail.com>
2019-11-14 00:26:19 +0100

    Keep ant's visited cities in set

--- hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolution.java
+++ hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolution.java
@@ -1,7 +1,9 @@
 package hr.fer.zemris.optjava.dz6;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TSPSolution {
 
@@ -9,7 +11,10 @@ public class TSPSolution {
 
     public List<City> route;
 
+    public Set<City> visited;
+
     public TSPSolution(int cityCount) {
         route = new ArrayList<>(cityCount);
+        visited = new HashSet<>();
     }
 }
