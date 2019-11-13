a0aacb6373fbce3db1381a900c0638f94603e58d
yetra <brdujmovic@gmail.com>
2019-11-14 00:24:40 +0100

    Replace TSPSolution#cityIndexes with route

--- hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolution.java
+++ hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolution.java
@@ -1,12 +1,15 @@
 package hr.fer.zemris.optjava.dz6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TSPSolution {
 
     public int tourLength;
 
-    public int[] cityIndexes;
+    public List<City> route;
 
     public TSPSolution(int cityCount) {
-        cityIndexes = new int[cityCount];
+        route = new ArrayList<>(cityCount);
     }
 }
