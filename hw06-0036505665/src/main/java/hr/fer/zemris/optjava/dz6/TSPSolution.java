8ef6b7e9a7e9f28bde05d15af6487a803fe53cd2
yetra <brdujmovic@gmail.com>
2019-11-14 00:33:44 +0100

    Add TSPSolution javadoc

--- hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolution.java
+++ hw06-0036505665/src/main/java/hr/fer/zemris/optjava/dz6/TSPSolution.java
@@ -5,32 +5,74 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Models an ant for solving the TSP.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public class TSPSolution {
 
+    /**
+     * The length of this ant's tour.
+     */
     public int tourLength;
 
+    /**
+     * The tour of cities.
+     */
     public List<City> tour;
 
+    /**
+     * A set of visited cities.
+     */
     public Set<City> visited;
 
+    /**
+     * Constructs an {@link TSPSolution}.
+     *
+     * @param cityCount the number of TSP cities
+     */
     public TSPSolution(int cityCount) {
         tour = new ArrayList<>(cityCount);
         visited = new HashSet<>();
     }
 
+    /**
+     * Visits the given city.
+     *
+     * @param city the city to visit
+     */
     public void visit(City city) {
         tour.add(city);
         visited.add(city);
     }
 
+    /**
+     * Returns {@code true} if the given city has been visited by this ant.
+     *
+     * @param city the city to check
+     * @return {@code true} if the given city has been visited by this ant
+     */
     public boolean visited(City city) {
         return visited.contains(city);
     }
 
+    /**
+     * Returns {@code true} if each city in the given list has been visited by this ant.
+     *
+     * @param cities the list of cities to check
+     * @return {@code true} if each city in the given list has been visited by this ant
+     */
     public boolean visitedAll(List<City> cities) {
         return visited.containsAll(cities);
     }
 
+    /**
+     * Returns the first city in this ant's tour.
+     *
+     * @return the first city in this ant's tour
+     */
     public City getInitialCity() {
         return tour.get(0);
     }
