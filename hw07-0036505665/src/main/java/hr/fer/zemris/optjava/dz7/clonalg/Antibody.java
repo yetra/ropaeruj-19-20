c4466f9b942fd65809870e94798aa83dcc257925
yetra <brdujmovic@gmail.com>
2019-12-04 23:10:50 +0100

    Add Antibody class

---
+++ hw07-0036505665/src/main/java/hr/fer/zemris/optjava/dz7/clonalg/Antibody.java
@@ -0,0 +1,77 @@
+package hr.fer.zemris.optjava.dz7.clonalg;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Models a {@link ClonAlg} antibody.
+ *
+ * @author Bruna Dujmović
+ * 
+ */
+public class Antibody {
+
+    /**
+     * The variables of this antibody.
+     */
+    double[] variables;
+
+    /**
+     * The value (fitness/error) of this antibody.
+     */
+    double value;
+
+    /**
+     * Constructs an {@link Antibody} of the given dimensions with all its variables set to zero.
+     *
+     * @param dimensions the dimensions of the solution
+     */
+    public Antibody(int dimensions) {
+        variables = new double[dimensions];
+    }
+
+    /**
+     * Constructs an {@link Antibody} of the given dimensions with random variable values.
+     *
+     * @param dimensions the dimensions of the solution
+     * @param mins the lowest allowed value for each antibody variable
+     * @param maxs the highest allowed value for each antibody variable
+     */
+    public Antibody(int dimensions, double[] mins, double[] maxs) {
+        this(dimensions);
+
+        randomize(mins, maxs);
+    }
+
+    /**
+     * Constructs an {@link Antibody} of the given variables and value.
+     *
+     * @param variables the variables of this antibody
+     * @param value the value (fitness/error) of this antibody
+     */
+    private Antibody(double[] variables, double value) {
+        this.variables = variables;
+        this.value = value;
+    }
+
+    /**
+     * Randomizes the variables of this antibody.
+     *
+     * @param mins the lowest allowed value for each antibody variable
+     * @param maxs the highest allowed value for each antibody variable
+     */
+    public void randomize(double[] mins, double[] maxs) {
+        for (int i = 0; i < variables.length; i++) {
+            variables[i] = ThreadLocalRandom.current().nextDouble(mins[i], maxs[i]);
+        }
+    }
+
+    /**
+     * Returns a deep copy of this antibody.
+     *
+     * @return a deep copy of this antibody
+     */
+    public Antibody copy() {
+        return new Antibody(Arrays.copyOf(variables, variables.length), value);
+    }
+}
