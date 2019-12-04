eb1b9ffd65a8cf87be07f341227acb5d4e5f2fc7
yetra <brdujmovic@gmail.com>
2019-12-05 00:53:38 +0100

    Expand ReadOnlyDataset interface

--- hw07-0036505665/src/main/java/hr/fer/zemris/optjava/dz7/ReadOnlyDataset.java
+++ hw07-0036505665/src/main/java/hr/fer/zemris/optjava/dz7/ReadOnlyDataset.java
@@ -2,18 +2,46 @@ package hr.fer.zemris.optjava.dz7;
 
 /**
  * An interface representing a read-only dataset.
+ *
+ * @author Bruna Dujmović
+ * 
  */
 public interface ReadOnlyDataset {
-    /*
-     TODO Razred čiji se ovdje predaje objekt trebao bi ponuditi metode poput:
-     koliko ima uzoraka za učenje, koliko uzorak ima ulaza, koliko ima izlaza,
-     dohvat i-tog uzorka (moguće čak i parcijalno: posebno ulaznog dijela, posebno izlaznog dijela) i slično.
-     */
 
     /**
      * Returns the number of samples in this dataset.
      *
      * @return the number of samples in this dataset
      */
-    int getNumberOfSamples();
+    int getSamplesCount();
+
+    /**
+     * Returns the number of inputs in each sample in this dataset.
+     *
+     * @return the number of inputs in each sample in this dataset
+     */
+    int getInputsCount();
+
+    /**
+     * Returns the number of outputs in each sample in this dataset.
+     *
+     * @return the number of outputs in each sample in this dataset
+     */
+    int getOutputsCount();
+
+    /**
+     * Returns the index-th input in this dataset.
+     *
+     * @param index the index of the input to get
+     * @return the index-th input in this dataset
+     */
+    double[] getInput(int index);
+
+    /**
+     * Returns the index-th output in this dataset.
+     *
+     * @param index the index of the output to get
+     * @return the index-th output in this dataset
+     */
+    boolean[] getOutput(int index);
 }
