29b04357e4641bc67082387573442c6fd3abeca0
yetra <brdujmovic@gmail.com>
2019-12-05 06:55:10 +0100

    Add ErrorFunction class

---
+++ hw07-0036505665/src/main/java/hr/fer/zemris/optjava/dz7/function/ErrorFunction.java
@@ -0,0 +1,57 @@
+package hr.fer.zemris.optjava.dz7.function;
+
+import hr.fer.zemris.optjava.dz7.ReadOnlyDataset;
+import hr.fer.zemris.optjava.dz7.nn.FFANN;
+
+/**
+ * Models an error function that determines how wrong a neural network's outputs are
+ * compared to the original dataset.
+ *
+ * @author Bruna Dujmović
+ * 
+ */
+public class ErrorFunction implements Function {
+
+    /**
+     * The neural network used for calculating the error.
+     */
+    private FFANN neuralNetwork;
+
+    /**
+     * The dataset used for calculating the error.
+     */
+    private ReadOnlyDataset dataset;
+
+    /**
+     * Constructs an {@link ErrorFunction}.
+     *
+     * @param neuralNetwork the neural network used for calculating the error
+     * @param dataset the dataset used for calculating the error
+     */
+    public ErrorFunction(FFANN neuralNetwork, ReadOnlyDataset dataset) {
+        this.neuralNetwork = neuralNetwork;
+        this.dataset = dataset;
+    }
+
+    @Override
+    public int getDimensions() {
+        return dataset.getOutputsCount();
+    }
+
+    @Override
+    public double valueAt(double[] point) {
+        double value = 0;
+
+        for (int s = 0, samplesCount = dataset.getSamplesCount(); s < samplesCount; s++) {
+            double[] sampleOutputs = dataset.getOutput(s);
+            double[] nnOutputs = new double[getDimensions()];
+            neuralNetwork.calculateOutputs(dataset.getInput(s), point, nnOutputs);
+
+            for (int o = 0, outputsCount = dataset.getOutputsCount(); o < outputsCount; o++) {
+                value += Math.pow(sampleOutputs[o] - nnOutputs[o], 2);
+            }
+        }
+
+        return value / dataset.getSamplesCount();
+    }
+}
