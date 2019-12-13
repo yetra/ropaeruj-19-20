b2e4f2c1fd2db10c9c8353f01665a020f3a149cf
Bruna <brdujmovic@gmail.com>
2019-12-13 19:51:21 +0100

    Add hw07 Function interface

---
+++ hw08-0036505665/src/main/java/hr/fer/zemris/optjava/dz8/function/Function.java
@@ -0,0 +1,25 @@
+package hr.fer.zemris.optjava.dz8.function;
+
+/**
+ * Models an arbitrary function of n real variables.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public interface Function {
+
+    /**
+     * Returns the number of dimensions of this function.
+     *
+     * @return the number of dimensions of this function
+     */
+    int getDimensions();
+
+    /**
+     * Returns the value of this function at the specified point.
+     *
+     * @param point the point for calculating the value
+     * @return the value of this function in the specified point
+     */
+    double valueAt(double[] point);
+}
