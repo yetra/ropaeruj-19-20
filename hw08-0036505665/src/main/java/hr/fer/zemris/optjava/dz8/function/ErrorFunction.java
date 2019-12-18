9354999040d6f9f4a9a5bcf4b0d141cc83fae5b7
Bruna <brdujmovic@gmail.com>
2019-12-19 00:56:28 +0100

    CUse a copy vector in ErrorFunction#valueAt method

--- hw08-0036505665/src/main/java/hr/fer/zemris/optjava/dz8/function/ErrorFunction.java
+++ hw08-0036505665/src/main/java/hr/fer/zemris/optjava/dz8/function/ErrorFunction.java
@@ -3,6 +3,8 @@ package hr.fer.zemris.optjava.dz8.function;
 import hr.fer.zemris.optjava.dz8.dataset.ReadOnlyDataset;
 import hr.fer.zemris.optjava.dz8.ann.ANN;
 
+import java.util.Arrays;
+
 /**
  * Models the mean squared error (MSE) function - used to determine how wrong
  * a neural network's outputs are compared to the original dataset.
@@ -40,12 +42,13 @@ public class ErrorFunction implements Function {
 
     @Override
     public double valueAt(double[] vector) {
+        double[] vectorCopy = Arrays.copyOf(vector, vector.length);
         double[] nnOutputs = new double[dataset.getOutputsCount()];
         double value = 0;
 
         for (int s = 0, samplesCount = dataset.getSamplesCount(); s < samplesCount; s++) {
             double[] sampleOutputs = dataset.getOutput(s);
-            neuralNetwork.calculateOutputs(dataset.getInput(s), nnOutputs, vector);
+            neuralNetwork.calculateOutputs(dataset.getInput(s), nnOutputs, vectorCopy);
 
             for (int o = 0, outputsCount = dataset.getOutputsCount(); o < outputsCount; o++) {
                 value += Math.pow(sampleOutputs[o] - nnOutputs[o], 2);
