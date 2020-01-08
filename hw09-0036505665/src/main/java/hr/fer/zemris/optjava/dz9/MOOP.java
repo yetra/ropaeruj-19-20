795d2c3e1107a2c80081481879000700ec326cff
yetra <brdujmovic@gmail.com>
2020-01-08 16:14:22 +0100

    Add MOOP main program

---
+++ hw09-0036505665/src/main/java/hr/fer/zemris/optjava/dz9/MOOP.java
@@ -0,0 +1,59 @@
+package hr.fer.zemris.optjava.dz9;
+
+import hr.fer.zemris.optjava.dz9.crossover.Crossover;
+import hr.fer.zemris.optjava.dz9.crossover.OnePointCrossover;
+import hr.fer.zemris.optjava.dz9.mutation.GaussianMutation;
+import hr.fer.zemris.optjava.dz9.mutation.Mutation;
+import hr.fer.zemris.optjava.dz9.problem.MOOPProblem;
+import hr.fer.zemris.optjava.dz9.problem.Problem1;
+import hr.fer.zemris.optjava.dz9.problem.Problem2;
+import hr.fer.zemris.optjava.dz9.selection.RouletteWheelSelection;
+import hr.fer.zemris.optjava.dz9.selection.Selection;
+
+/**
+ * A program for running the Non-Dominated Sorting Genetic Algorithm (NSGA) on a specified {@link MOOPProblem}.
+ *
+ * Usage examples:
+ * 2 100 decision-space 1000
+ * 1 100 objective-space 2500
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class MOOP {
+
+    /**
+     * Executes the NSGA on the specified optimization problem.
+     *
+     * @param args the command-line arguments, 4 expected:
+     *             problemNumber - the number of the MOOP problem to use (1 / 2),
+     *             populationSize - the size of the population,
+     *             distanceCalculationString - the distance calculation method (decision-space / objective-space),
+     *             maxIterations - the maximum number of algorithm iterations
+     */
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("Expected 4 arguments, got " + args.length);
+            System.exit(1);
+        }
+
+        MOOPProblem problem = getProblem(args[0]);
+        int populationSize = Integer.parseInt(args[1]);
+        boolean decisionSpaceDistance = getDistanceCalculationType(args[2]);
+        int maxIterations = Integer.parseInt(args[3]);
+
+        Crossover crossover = new OnePointCrossover(0.9);
+        Mutation mutation = new GaussianMutation(0.03, 1);
+        Selection selection = new RouletteWheelSelection();
+
+        new NSGA(problem, populationSize, maxIterations, decisionSpaceDistance, crossover, mutation, selection).run();
+    }
+
+    private static MOOPProblem getProblem(String problemNumber) {
+        return null;
+    }
+
+    private static boolean getDistanceCalculationType(String distanceCalculationString) {
+        return false;
+    }
+}
