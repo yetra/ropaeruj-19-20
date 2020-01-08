854e150cf51ec41b47af48eb55142bede025577a
yetra <brdujmovic@gmail.com>
2020-01-08 15:07:58 +0100

    Add MOOPProblem interface

---
+++ hw09-0036505665/src/main/java/hr/fer/zemris/optjava/dz9/problem/MOOPProblem.java
@@ -0,0 +1,37 @@
+package hr.fer.zemris.optjava.dz9.problem;
+
+/**
+ * An interface containing the methods used to describe an MOOP problem.
+ *
+ * Each {@link MOOPProblem} class should know the dimensions of its objective and solution spaces,
+ * and should be able to evaluate a given solution.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public interface MOOPProblem {
+
+    /**
+     * Returns the dimensions of the objective space.
+     *
+     * @return the dimensions of the objective space
+     */
+    int getNumberOfObjectives();
+
+    /**
+     * Returns the dimensions of the solution space.
+     *
+     * @return the dimensions of the solution space
+     */
+    int getNumberOfVariables();
+
+    /**
+     * Evaluates the given solution.
+     *
+     * @param solution the solution to evaluate
+     * @param objectives the array that will contain the evaluation results
+     * @throws IllegalArgumentException if any array is of incorrect length
+     */
+    void evaluate(double[] solution, double[] objectives);
+}
+
