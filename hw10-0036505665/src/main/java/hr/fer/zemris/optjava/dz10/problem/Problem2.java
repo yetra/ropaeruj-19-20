48f21b8365c6af8d5a19d5b72dc4ed39b9e2aab4
yetra <brdujmovic@gmail.com>
2020-01-09 23:27:46 +0100

    Keep track of objective mins & maxs in Problem2

--- hw10-0036505665/src/main/java/hr/fer/zemris/optjava/dz10/problem/Problem2.java
+++ hw10-0036505665/src/main/java/hr/fer/zemris/optjava/dz10/problem/Problem2.java
@@ -23,6 +23,17 @@ public class Problem2 implements MOOPProblem {
      */
     private static final double[] MAXS = new double[] {1, 5};
 
+
+    /**
+     * The lowest possible values of each objective.
+     */
+    private double[] objectiveMins = new double[2];
+
+    /**
+     * The highest possible values of each objective.
+     */
+    private double[] objectiveMaxs = new double[2];
+
     @Override
     public int getNumberOfObjectives() {
         return 2;
@@ -44,12 +55,40 @@ public class Problem2 implements MOOPProblem {
     }
 
     @Override
+    public double[] getObjectiveMins() {
+        return objectiveMins;
+    }
+
+    @Override
+    public double[] getObjectiveMaxs() {
+        return objectiveMaxs;
+    }
+
+
+    @Override
     public void evaluate(Solution solution) {
         if (solution.variables.length != 2 || solution.objectives.length != 2) {
             throw new IllegalArgumentException("Solution variables and objectives need to be of length 2!");
         }
 
         solution.objectives[0] = solution.variables[0];
+        checkObjectiveBounds(solution.objectives[0], 0);
+
         solution.objectives[1] = (1 + solution.variables[1]) / solution.variables[0];
+        checkObjectiveBounds(solution.objectives[1], 1);
+    }
+
+    /**
+     * Checks and updates {@link #objectiveMins} and {@link #objectiveMaxs} based on the given objective.
+     *
+     * @param objective the objective to check
+     * @param index the index of the objective to check
+     */
+    private void checkObjectiveBounds(double objective, int index) {
+        if (objective < objectiveMins[index]) {
+            objectiveMins[index] = objective;
+        } else if (objective > objectiveMaxs[index]) {
+            objectiveMaxs[index] = objective;
+        }
     }
 }
