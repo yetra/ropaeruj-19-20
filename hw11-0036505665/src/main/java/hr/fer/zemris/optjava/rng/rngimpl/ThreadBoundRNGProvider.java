68c37d1cac176d988d618c83780cc1d22888cad7
yetra <brdujmovic@gmail.com>
2020-01-20 19:55:49 +0100

    Add ThreadBoundRNGProvider class

---
+++ hw11-0036505665/src/main/java/hr/fer/zemris/optjava/rng/rngimpl/ThreadBoundRNGProvider.java
@@ -0,0 +1,19 @@
+package hr.fer.zemris.optjava.rng.rngimpl;
+
+import hr.fer.zemris.optjava.rng.IRNG;
+import hr.fer.zemris.optjava.rng.IRNGProvider;
+
+/**
+ * An implementation of {@link IRNGProvider} which assumes that
+ * the current thread also implements {@link IRNGProvider}.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class ThreadBoundRNGProvider implements IRNGProvider {
+
+    @Override
+    public IRNG getRNG() {
+        return ((IRNGProvider) Thread.currentThread()).getRNG();
+    }
+}
