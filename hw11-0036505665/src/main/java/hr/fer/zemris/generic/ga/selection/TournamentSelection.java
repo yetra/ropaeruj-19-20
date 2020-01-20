4577e213bf0e43b8177ac89756f3c8d45f502504
yetra <brdujmovic@gmail.com>
2020-01-20 20:55:42 +0100

    Implement tournament selection

---
+++ hw11-0036505665/src/main/java/hr/fer/zemris/generic/ga/selection/TournamentSelection.java
@@ -0,0 +1,62 @@
+package hr.fer.zemris.generic.ga.selection;
+
+import hr.fer.zemris.generic.ga.GASolution;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * An implementation of tournament selection.
+ *
+ * The size of the tournament is specified using the {@link #tournamentSize} field.
+ * For example, if {@link #tournamentSize} is 4 then 4 chromosomes will be chosen from
+ * the population to form a tournament, and the best one among them will be selected.
+ *
+ * This implementation makes sure that all chromosomes in a tournament are unique.
+ *
+ * @param <T> the type of the solution's values
+ * @author Bruna Dujmović
+ *
+ */
+public class TournamentSelection<T> implements ISelection<T> {
+
+    /**
+     * The minimum tournament size.
+     */
+    private static final int MIN_TOURNAMENT_SIZE = 2;
+
+    /**
+     * The size of the tournament.
+     */
+    private int tournamentSize;
+
+    /**
+     * Constructs a {@link TournamentSelection} of a given size.
+     *
+     * @param tournamentSize the size of the tournament
+     * @throws IllegalArgumentException if the size is less than the minimum size
+     */
+    public TournamentSelection(int tournamentSize) {
+        if (tournamentSize < MIN_TOURNAMENT_SIZE) {
+            throw new IllegalArgumentException("Tournament size must be >= " + MIN_TOURNAMENT_SIZE);
+        }
+
+        this.tournamentSize = tournamentSize;
+    }
+
+    @Override
+    public GASolution<T> from(Collection<GASolution<T>> population) {
+        List<GASolution<T>> populationList = new ArrayList<>(population);
+        List<GASolution<T>> tournament = new ArrayList<>();
+
+        while (tournament.size() < tournamentSize) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(population.size());
+            tournament.add(populationList.get(randomIndex));
+        }
+
+        return Collections.min(tournament);
+    }
+}
