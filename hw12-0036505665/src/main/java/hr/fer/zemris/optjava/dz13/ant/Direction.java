eb4a257cebfcc51adf303575939db2b7106cc9e1
yetra <brdujmovic@gmail.com>
2020-02-05 16:06:13 +0100

    Add Direction enum

---
+++ hw12-0036505665/src/main/java/hr/fer/zemris/optjava/dz13/ant/Direction.java
@@ -0,0 +1,30 @@
+package hr.fer.zemris.optjava.dz13.ant;
+
+/**
+ * An enumeration of all possible ant directions (orientations.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public enum Direction {
+
+    /**
+     * Up.
+     */
+    NORTH,
+
+    /**
+     * Down.
+     */
+    SOUTH,
+
+    /**
+     * Right.
+     */
+    EAST,
+
+    /**
+     * Left.
+     */
+    WEST
+}
